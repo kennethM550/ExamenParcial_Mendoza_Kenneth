0331618b502c3711b82f597d4a5897a11c1e80da
kennethM550 <kenneth.mendoza@unmsm.edu.pe>
2021-12-06 22:29:42 -0500

    Avance 4

---
+++ src/modelo/Operación.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Kenneth
+ */
+public class Operación {
+   private Date fecha=new Date();
+   private float monto;
+   
+   public boolean registrar(Date fecha,float monto){
+       boolean result= false;
+       
+       return result;
+   }
+}
