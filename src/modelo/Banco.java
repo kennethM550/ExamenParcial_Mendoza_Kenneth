aa10f037344177dc0ff0d2108a3b57a9ba720582
kennethM550 <kenneth.mendoza@unmsm.edu.pe>
2021-12-06 21:23:38 -0500

    Clases

---
+++ src/modelo/Banco.java
@@ -0,0 +1,35 @@
+
+package modelo;
+
+
+public class Banco {
+    private String Ruc;
+    private String nombre;
+    private String dirección; 
+
+    public String getRuc() {
+        return Ruc;
+    }
+
+    public void setRuc(String Ruc) {
+        this.Ruc = Ruc;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDirección() {
+        return dirección;
+    }
+
+    public void setDirección(String dirección) {
+        this.dirección = dirección;
+    }
+    
+    
+}
